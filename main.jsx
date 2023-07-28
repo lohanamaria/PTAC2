858aa8760194ee646bc84ce1419c4046182ac86d
lohanamaria <116275294+lohanamaria@users.noreply.github.com>
2023-07-28 21:08:03 +0000

    commit um

--- main.jsx
+++ main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-export function App() {
-  return (
-    <div>
-      <h1>Hello Word!</h1>
-    </div>
-
-  );
-}
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
